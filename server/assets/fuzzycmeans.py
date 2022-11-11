fcc0fc07f5f0eb121f1f9986218ed20433bdf0c6
johan <johangunawan818@gmail.com>
2022-11-12 00:17:52 +0700

    finish fuzzy c-means algorithm

---
+++ server/assets/fuzzycmeans.py
@@ -0,0 +1,150 @@
+from typing import Optional
+
+import numpy as np
+from numpy.typing import NDArray
+from pydantic import BaseModel, Extra, Field, validate_arguments
+import pandas as pd
+
+
+class FCM(BaseModel):
+    r"""Fuzzy C-means Model
+    Attributes:
+        n_clusters (int): The number of clusters to form as well as the number
+        of centroids to generate by the fuzzy C-means.
+        max_iter (int): Maximum number of iterations of the fuzzy C-means
+        algorithm for a single run.
+        m (float): Degree of fuzziness: $m \in (1, \infty)$.
+        error (float): Relative tolerance with regards to Frobenius norm of
+        the difference
+        in the cluster centers of two consecutive iterations to declare
+        convergence.
+        random_state (Optional[int]): Determines random number generation for
+        centroid initialization.
+        Use an int to make the randomness deterministic.
+        trained (bool): Variable to store whether or not the model has been
+        trained.
+    Returns:
+        FCM: A FCM model.
+    Raises:
+        ReferenceError: If called without the model being trained
+    """
+
+    class Config:
+        extra = Extra.allow
+        arbitrary_types_allowed = True
+
+    n_clusters: int = Field(5, ge=1, le=100)
+    max_iter: int = Field(150, ge=1, le=1000)
+    m: float = Field(2.0, ge=1.0)
+    error: float = Field(1e-5, ge=1e-9)
+    random_state: Optional[int] = None
+    trained: bool = Field(False, const=True)
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def fit(self, X: NDArray) -> None:
+        """Train the fuzzy-c-means model
+        Args:
+            X (NDArray): Training instances to cluster.
+        """
+        
+        self.rng = np.random.default_rng(self.random_state)
+        n_samples = X.shape[0]
+        self.u = self.rng.uniform(size=(n_samples, self.n_clusters))
+        self.u = np.divide(self.u,np.tile(self.u.sum(axis=1)[np.newaxis].T,self.n_clusters))
+        
+        print(np.tile(self.u.sum(axis=1)[np.newaxis].T,self.n_clusters))
+
+        for _ in range(self.max_iter):
+            u_old = self.u.copy()
+            self._centers = FCM._next_centers(X, self.u, self.m)
+            self.u = self.soft_predict(X)
+            print(self.error)
+            # Stopping rule
+            if np.linalg.norm(self.u - u_old) < self.error:
+                break
+        self.trained = True
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def soft_predict(self, X: NDArray) -> NDArray:
+        """Soft predict of FCM
+        Args:
+            X (NDArray): New data to predict.
+        Returns:
+            NDArray: Fuzzy partition array, returned as an array with
+            n_samples rows and n_clusters columns.
+        """
+        temp = FCM._dist(X, self._centers) ** (2 / (self.m - 1))
+        denominator_ = temp.reshape((X.shape[0], 1, -1)).repeat(
+            temp.shape[-1], axis=1
+        )
+        denominator_ = temp[:, :, np.newaxis] / denominator_
+        return 1 / denominator_.sum(2)
+
+    @validate_arguments(config=dict(arbitrary_types_allowed=True))
+    def predict(self, X: NDArray) -> NDArray:
+        """Predict the closest cluster each sample in X belongs to.
+        Args:
+            X (NDArray): New data to predict.
+        Raises:
+            ReferenceError: If it called without the model being trained.
+        Returns:
+            NDArray: Index of the cluster each sample belongs to.
+        """
+        if self._is_trained():
+            X = np.expand_dims(X, axis=0) if len(X.shape) == 1 else X
+            return self.soft_predict(X).argmax(axis=-1)
+        raise ReferenceError(
+            "You need to train the model. Run `.fit()` method to this."
+        )
+
+    def _is_trained(self) -> bool:
+        if self.trained:
+            return True
+        return False
+
+    @staticmethod
+    def _dist(A: NDArray, B: NDArray) -> NDArray:
+        """Compute the euclidean distance two matrices"""
+        return np.sqrt(np.einsum("ijk->ij", (A[:, None, :] - B) ** 2))
+
+    @staticmethod
+    def _next_centers(X: NDArray, u: NDArray, m: float):
+        """Update cluster centers"""
+        
+        um = u**m
+        return (X.T @ um / np.sum(um, axis=0)).T
+
+    @property
+    def centers(self) -> NDArray:
+        if self._is_trained():
+            return self._centers
+        raise ReferenceError(
+            "You need to train the model. Run `.fit()` method to this."
+        )
+
+    @property
+    def partition_coefficient(self) -> float:
+        """Partition coefficient
+        Equation 12a of
+        [this paper](https://doi.org/10.1016/0098-3004(84)90020-7).
+        """
+        if self._is_trained():
+            return np.mean(self.u**2)
+        raise ReferenceError(
+            "You need to train the model. Run `.fit()` method to this."
+        )
+
+    @property
+    def partition_entropy_coefficient(self):
+        if self._is_trained():
+            return -np.mean(self.u * np.log2(self.u))
+        raise ReferenceError(
+            "You need to train the model. Run `.fit()` method to this."
+        )
+
+
+data = pd.read_csv("ratings.csv", delimiter=',', usecols= [2])
+X = data.values
+
+fcm = FCM(n_cluster=5)
+fcm.fit(X)
--- server/controller/cluster.js
+++ server/controller/cluster.js
@@ -1,6 +1,8 @@
 const { TOTAL_CLUSTER } = require('../constants/general');
 const { Movie, Rating, Genre } = require('../models');
 const  { OK, INTERNAL_ERROR, NOT_FOUND  } = require('../constants/responseCode');
+const math = require('mathjs');
+const tf = require('@tensorflow/tfjs');
 
 const mappingMovies = (movies) => {
     const mapping = {};
@@ -12,7 +14,7 @@ const mappingMovies = (movies) => {
     return mapping;
 }
 
-
+//fuzzy c-means
 const FCM = async (req, res) => {
     try {
         const ratings = await Rating.findAll();
@@ -21,10 +23,13 @@ const FCM = async (req, res) => {
         // const genres = await Genre.findAll();
 
         //Generate nilai random matriks untuk jumlah data x cluster
-        const totalCluster = 3; //total rating 0.5 1 1.5 2 ....
+        const epsilon = 1e-5;
+        const maxIter = 150;
+        const totalCluster = 5; //total rating 0.5 1 1.5 2 ....
         const initialPointData = {};
         const randomMatrix = {}; //cluster x jumlah data
         const clusterUsers = {};
+        const matrixDivided = {};
         for(let i=0; i<ratings.length;i++){
             const userId = ratings[i]['UserId'];
             const rating = ratings[i]['Rating'];
@@ -34,48 +39,41 @@ const FCM = async (req, res) => {
                 initialPointData[userId] = {};
                 randomMatrix[userId] = {};
                 clusterUsers[userId] = {};
+                matrixDivided[userId] = {};
             }
 
             if(!initialPointData[userId][movieId]){
                 initialPointData[userId][movieId] = [];
                 randomMatrix[userId][movieId] = [];
                 clusterUsers[userId][movieId] = {};
+                matrixDivided[userId][movieId] = null;
             }
 
             initialPointData[userId][movieId] = rating;
-            const tempArr = new Array(totalCluster);
+            let tempArr = [];
             let maximum = 1;
-            let totalFilled = 1;
+            let sumRowRandomNumber = 0;
             for(let j=0; j< totalCluster;j++){
-                let randomIndex =  Math.floor(Math.random() * totalCluster);
-                while(tempArr[randomIndex] != undefined && totalFilled != totalCluster){
-                    randomIndex = Math.floor(Math.random() * totalCluster);
-                    if(tempArr[randomIndex] == undefined){
-                        totalFilled++;
-                    }
-                }
-
-                const randomNumber = Math.random() * maximum;
-                maximum -= randomNumber;
-                tempArr[randomIndex] = randomNumber.toFixed(3);
+                const randomNumber = parseFloat((Math.random() * maximum).toFixed(5));
+                tempArr.push(randomNumber);
+                sumRowRandomNumber += randomNumber
             }
-
+            tempArr = tempArr.map(value => value/sumRowRandomNumber);
+            randomMatrix[userId][movieId].push(...tempArr);
             const min = Math.min(...tempArr);
 
             clusterUsers[userId][movieId] = { 
-                cluster: 'C' + (tempArr.findIndex( value => value == min) + 1),
+                cluster: 'C' + (randomMatrix[userId][movieId].findIndex( value => value == min) + 1),
                 value : min 
-            };
-            randomMatrix[userId][movieId].push(...tempArr);
+            };   
         }
-        
 
         let start = true;
         let iteration = 1;
-        const clusters = {};
+        const centroids = {};
         const euclidiens = {};
         const m = 3;
-        while(start){
+        for(let iter=0;iter<maxIter;iter++){
             console.log('Iterasi ',iteration);
             // Hitung centroid
             for(let j=0;j<totalCluster;j++){
@@ -93,10 +91,8 @@ const FCM = async (req, res) => {
                     penyebut += Uijsquare;
                 }
 
-                clusters['C'+(j+1)] = pembilang/penyebut;
+                centroids['C'+(j+1)] = pembilang/penyebut;
             }
-
-            console.log(clusters);
 
             //Euclidien Distance
             for(let i=0;i<ratings.length;i++){
@@ -111,34 +107,35 @@ const FCM = async (req, res) => {
                     euclidiens[userId][movieId] = {};
                 }
                 
-                for(let c=0;c<Object.keys(clusters).length;c++){
-                    euclidiens[userId][movieId]['C'+(c+1)] = Math.sqrt(Math.pow(clusters['C'+(c+1)] - initialPointData[userId][movieId],2));
+                for(let c=0;c<Object.keys(centroids).length;c++){
+                    euclidiens[userId][movieId]['C'+(c+1)] = Math.sqrt(Math.pow(centroids['C'+(c+1)] - initialPointData[userId][movieId],2));
                 }
             }
 
-            const oldRandommatrix = randomMatrix;
+            const matrixError = [];
+            
             //update bilangan matrix 
             for(let c=0;c<totalCluster;c++){
+                matrixError[c] = [];
                 for(let i=0;i<ratings.length;i++){
                     const userId = ratings[i]['UserId'];
                     const movieId = ratings[i]['MovieId'];
 
                     // let pembilang = Math.pow((1/euclidiens[userId][movieId]['C'+(c+1)]),1/(m-1));
                     let penyebut = 0;
-                    
+                    let temp = randomMatrix[userId][movieId][c];
                     for(let k=0;k<totalCluster;k++){
                         penyebut += Math.pow((euclidiens[userId][movieId]['C'+(c+1)]/euclidiens[userId][movieId]['C'+(k+1)]),2/(m-1));
                     }
 
                     randomMatrix[userId][movieId][c] = 1/penyebut;
+                    temp = randomMatrix[userId][movieId][c] - temp;
+                    matrixError[c].push(temp);
                 }
             }
-        
 
             //Check apakah ada perpindahan cluster atau tidak
             // jika tidak ada perpindahan cluster lagi maka iterasi berhenti
-
-            const oldClusterUsers = clusterUsers;
             let stop = true;
             for(let i=0;i<ratings.length;i++){
                 const userId = ratings[i]['UserId'];
@@ -150,25 +147,24 @@ const FCM = async (req, res) => {
                     cluster: 'C' + (randomMatrix[userId][movieId].findIndex( value => value == min) + 1),
                     value : min 
                 };
-
-                if(oldClusterUsers[userId][movieId]['cluster'] != clusterUsers[userId][movieId]['cluster']){
-                    console.log("Tidak sama");
-                    stop = false;
-                }
             }
-            if(stop){
-                start = false;
+            let Norm = tf.tensor2d(matrixError);
+            console.log(Norm.norm().dataSync()[0] < epsilon);
+            console.log(Norm.norm().dataSync()[0]);
+            if(Norm.norm().dataSync()[0] < epsilon){
+                break;
             }
             iteration++;
         }
 
-        console.log(clusters);
-        res.status(OK).send(JSON.stringify(randomMatrix));
+        res.status(OK).send(JSON.stringify(centroids));
 
     } catch (err) {
         console.log(err);
         res.sendStatus(INTERNAL_ERROR);
     }
 }
+
+//item based collaborative filtering
 
 module.exports = { FCM }
